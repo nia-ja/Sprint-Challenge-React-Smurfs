4d08d1c994b6d853cf69bbbbf20a939a4bb2bc11
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-04-06 10:43:31 -0700

    Added basic styling, changed Links to NavLinks, added activeClassName

--- village/src/components/Navigation.js
+++ village/src/components/Navigation.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <div>
       <nav className="navigation">
-        <h1>SMURFS App</h1>
-        <div>
-          <Link to="/">Smurfs List</Link>
-        </div>
-        <div>
-          <Link to="/smurf-form">Add Smurf</Link>
+        <h1>Smurf Village</h1>
+        <div className='links'>
+          <button className='link'>
+            <NavLink exact to="/" activeClassName='activeNavButton'>Smurfs List</NavLink>
+          </button>
+          <button className='link'>
+            <NavLink to="/smurf-form" activeClassName='activeNavButton'>Add Smurf</NavLink>
+          </button>
         </div>
       </nav>
     </div>
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -6,20 +6,17 @@ class Smurfs extends Component {
   render() {
     return (
       <div className="Smurfs">
-        <h1>Smurf Village</h1>
-        <ul>
-          {this.props.smurfs.map(smurf => {
-            return (
-              <Smurf
-                name={smurf.name}
-                id={smurf.id}
-                age={smurf.age}
-                height={smurf.height}
-                key={smurf.id}
-              />
-            );
-          })}
-        </ul>
+        {this.props.smurfs.map(smurf => {
+          return (
+            <Smurf
+              name={smurf.name}
+              id={smurf.id}
+              age={smurf.age}
+              height={smurf.height}
+              key={smurf.id}
+            />
+          );
+        })}
       </div>
     );
   }
